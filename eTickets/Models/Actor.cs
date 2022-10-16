664d487b8731fea13c8d14d95738d1b23e50acdd
DQP <79057941+corvinus-qq6asm@users.noreply.github.com>
2022-10-16 14:37:22 +0200

    app_db_context

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL{ get; set; }
+        public int FullName { get; set; }
+        public int Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies{ get; set; }
+    }
+}
